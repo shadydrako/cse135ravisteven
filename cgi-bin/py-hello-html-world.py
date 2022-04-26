c103a5db88d4444cf669f655420720a9997c2a20
shadydrako <rtapia@ucsd.edu>
2022-04-26 12:54:16 -0700

    adding current date and time to html world

--- cgi-bin/py-hello-html-world.py
+++ cgi-bin/py-hello-html-world.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+from datetime import datetime
 
+time = datetime.now()
 print ("Content-type:text/html\r\n\r\n")
 print ('<html>')
 print ('<head>')
-print ('<title>Hello World - First CGI Program</title>')
+print ('<title> Hello World - Python!! </title>')
 print ('</head>')
 print ('<body>')
 print ('<h2>Hello World! This is my first CGI program</h2>')
+print ('<p>')
+print (time.strftime("%H:%M:%S"))
+print ('<p>')
 print ('</body>')
 print ('</html>')

6f76cfceaf666fd9950faf27cca76cbb06c0a518
shadydrako <rtapia@ucsd.edu>
2022-04-26 13:15:22 -0700

    added shebang

--- cgi-bin/py-env.py
+++ cgi-bin/py-env.py
@@ -1,8 +1,7 @@
+#!/usr/bin/python3
 import os
-
-
 print ("Content-type:text/html\r\n\r\n")
 for name, value in os.environ.items():
     print('<p>')
-    print(f'{name}={value}')
+    print('{name}={value}')
     print('<p>')

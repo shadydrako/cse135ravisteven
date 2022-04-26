10461d1deaa7a1f4d3bce491fd185cc745841db4
shadydrako <rtapia@ucsd.edu>
2022-04-26 13:07:33 -0700

    json but python tho

---
+++ cgi-bin/py-json-world.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+#creating a python object
+from datetime import datetime
+import socket
+import json
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+
+
+time = datetime.now()
+ 
+message = {
+    "title": "Hello World by in JSON form in python",
+    "time": time.strftime("%H:%M:%S"),
+    "ip": (s.getsockname()[0])
+}
+
+
+message2 = json.dumps(message)
+
+
+print ("Content-type:text/html\r\n\r\n")
+print (message2)

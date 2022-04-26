83361f2d5a127191b24a9dc7a9c81155f7c91ff7
shadydrako <rtapia@ucsd.edu>
2022-04-26 12:40:45 -0700

    new hello world file python

--- cgi-bin/py-hello-html-world.py
+++ cgi-bin/py-hello-html-world.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python3
-print "Content-Type: text/html"
-print
-print """\
-<html>
-    <head>
-        <title> Python - Hello World </title>
-    </head>
-    <body>
-        Hello World BREH
-    </body>
-</html>
-"""
+#!/usr/bin/python
+
+print "Content-type:text/html\r\n\r\n"
+print '<html>'
+print '<head>'
+print '<title>Hello World - First CGI Program</title>'
+print '</head>'
+print '<body>'
+print '<h2>Hello World! This is my first CGI program</h2>'
+print '</body>'
+print '</html>'

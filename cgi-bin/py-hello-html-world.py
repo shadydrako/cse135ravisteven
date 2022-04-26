b0b6311ca10ed77161e313b392b03c19f1b82f3a
shadydrako <rtapia@ucsd.edu>
2022-04-26 12:58:33 -0700

    adding ip address getter to pythong hello world

--- cgi-bin/py-hello-html-world.py
+++ cgi-bin/py-hello-html-world.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 from datetime import datetime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+
 
 time = datetime.now()
 print ("Content-type:text/html\r\n\r\n")
@@ -11,6 +16,7 @@ print ('<body>')
 print ('<h2>Hello World! This is my first CGI program</h2>')
 print ('<p>')
 print (time.strftime("%H:%M:%S"))
+print (print(s.getsockname()[0]))
 print ('<p>')
 print ('</body>')
 print ('</html>')

735929729a75949ec02fd3131ce2517c8662f2e9
shadydrako <rtapia@ucsd.edu>
2022-04-26 13:16:21 -0700

    f

--- cgi-bin/py-env.py
+++ cgi-bin/py-env.py
@@ -3,5 +3,5 @@ import os
 print ("Content-type:text/html\r\n\r\n")
 for name, value in os.environ.items():
     print('<p>')
-    print('{name}={value}')
+    print(f'{name}={value}')
     print('<p>')

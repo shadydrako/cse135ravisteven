2fb476987bde5615c626b9b78c7b2c9c459b7b2b
shadydrako <rtapia@ucsd.edu>
2022-04-27 10:26:52 -0700

    QUERY STRING

--- cgi-bin/py-get-echo.py
+++ cgi-bin/py-get-echo.py
@@ -9,13 +9,18 @@ print ("Content-type:text/html\r\n\r\n")
 #path_info = request.META.get('PATH_INFO')
 URLQUERY = os.environ['QUERY_STRING']
 URL = 'https://cse135ravisteven.site/'+ os.environ['REQUEST_URI']
+QUERYs = urlparse(URL)
+
 print('<p>')
 print("RAW QUERY STRING:", URLQUERY)
-print("RAW QUERY STRING:", URL)
 print('</p>')
 
-for param in os.environ.keys():
-    print('<p>')
-    print(param,os.environ[param])
-    print('</p>')
+print('<p>')
+print("QUERY:", QUERYs)
+print('</p>')
+
+#for param in os.environ.keys():
+  #  print('<p>')
+   # print(param,os.environ[param])
+    #print('</p>')
 #test

2d80a6b321afe1de6d11b9080c6d063cef7a04f7
shadydrako <rtapia@ucsd.edu>
2022-04-27 10:15:12 -0700

    raw query string

--- cgi-bin/py-get-echo.py
+++ cgi-bin/py-get-echo.py
@@ -6,8 +6,8 @@ print ("Content-type:text/html\r\n\r\n")
 #urlparse('//www.cwi.nl:80/%7Eguido/Python.html')
 #path_info = request.META.get('PATH_INFO')
 URL = os.environ[QUERY_STRING]
-print(<p>)
+print('<p>')
 print("RAW QUERY STRING:", URL)
-print(</p>)
+print('</p>')
 
 #test

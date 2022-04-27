67f2580426faf17ba4138fb81186786a3500e42f
shadydrako <rtapia@ucsd.edu>
2022-04-27 16:30:21 -0700

    post echo python works

--- cgi-bin/py-post-echo.py
+++ cgi-bin/py-post-echo.py
@@ -9,6 +9,10 @@ import os
 #h1
 print ("Content-type:text/html\r\n\r\n")
 
+print('<h1>')
+print("POST ECHO PYTHON ")
+print('</h1>')
+
 print('<p>')
 print('<b>')
 print(os.environ['REQUEST_METHOD'])
@@ -16,10 +20,12 @@ print('</b>')
 print('</p>')
 
 
+print('<h1>')
+print("MESSAGE BODY ")
+print('</h1>')
 
 for line in sys.stdin.readline():
     print(line)
-    print("HELSSSP")
 print('</p>')
 
 

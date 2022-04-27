58e6ab5b13dd0796912a8abb4f2051f4e71f8c3e
shadydrako <rtapia@ucsd.edu>
2022-04-27 10:09:04 -0700

    I HOPE THIS SHIT

--- cgi-bin/py-get-echo.py
+++ cgi-bin/py-get-echo.py
@@ -1,8 +1,13 @@
-#!/usr/bin/python
-
+#!/usr/bin/python3
+from urllib.parse import urlparse
 import os
+print ("Content-type:text/html\r\n\r\n")
 
-print "Content-type: text/html\r\n\r\n";
-print "<font size=+1>Environment</font><\br>";
+#urlparse('//www.cwi.nl:80/%7Eguido/Python.html')
+#path_info = request.META.get('PATH_INFO')
 for param in os.environ.keys():
-   print "<b>%20s</b>: %s<\br>" % (param, os.environ[param])
+       print ('<p>')
+       print (param, os.environ[param])
+       print ('</p>')
+
+#test

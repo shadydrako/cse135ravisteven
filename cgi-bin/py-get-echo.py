02b1c4ea93916893e5015ec67fc305c01b643fa7
shadydrako <rtapia@ucsd.edu>
2022-04-27 09:55:47 -0700

    syntax fix

--- cgi-bin/py-get-echo.py
+++ cgi-bin/py-get-echo.py
@@ -5,6 +5,6 @@ print ("Content-type:text/html\r\n\r\n")
 #urlparse('//www.cwi.nl:80/%7Eguido/Python.html')
 #path_info = request.META.get('PATH_INFO')
 for param in os.environ.keys():
-       print "<b>%20s</b>: %s<\br>" % (param, os.environ[param])
+       print ("<b>%20s</b>: %s<\br>" % (param, os.environ[param]))
 
 #test

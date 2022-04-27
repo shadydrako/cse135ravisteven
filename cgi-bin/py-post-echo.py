6838c698d4bc3383b3f0fbe8903bab516587c373
shadydrako <rtapia@ucsd.edu>
2022-04-27 12:33:01 -0700

    wanted to see environment stuff

--- cgi-bin/py-post-echo.py
+++ cgi-bin/py-post-echo.py
@@ -1,22 +1,7 @@
 #!/usr/bin/python3
-import requests
-from requests.structures import CaseInsensitiveDict
-
-url = "https://cse135ravisteven.site/cgi-bin/py-post-echo.py"
-
-headers = CaseInsensitiveDict()
-headers["Content-Type"] = "application/json"
-
-data = '{"productId": 123456, "quantity": 100}'
-
-
-resp = requests.post(url, headers=headers, data=data)
-
-
-
+import os
 print ("Content-type:text/html\r\n\r\n")
-print('<p>')
-print(resp.status_code)
-print('</p>')
-
-
+for name, value in os.environ.items():
+    print('<p>')
+    print(f'{name}={value}')
+    print('<p>')

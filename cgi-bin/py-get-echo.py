5a06a0bdc2e44a52908c8bedf53ea7526d10c48e
shadydrako <rtapia@ucsd.edu>
2022-04-27 00:53:21 -0700

    py get echo test

--- cgi-bin/py-get-echo.py
+++ cgi-bin/py-get-echo.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 from urllib.parse import urlparse
-
-URL = request.META.get('HTTP_REFERER')
-
-parsedURL = urlparse(URL)
 print ("Content-type:text/html\r\n\r\n")
-print (<p>)
-print( parsedURL)
-print (</p>)
+
+urlparse('//www.cwi.nl:80/%7Eguido/Python.html')
+
+print(ParseResult(scheme='', netloc='www.cwi.nl:80', path='/%7Eguido/Python.html',
+            params='', query='', fragment=''))
+
+#urlparse('www.cwi.nl/%7Eguido/Python.html')
+#ParseResult(scheme='', netloc='', path='www.cwi.nl/%7Eguido/Python.html',
+           # params='', query='', fragment='')
+#urlparse('help/Python.html')
+#ParseResult(scheme='', netloc='', path='help/Python.html', params='',
+           # query='', fragment='')

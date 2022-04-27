a65e76c1dd15c4c981bbadd41fd272696645fffd
shadydrako <rtapia@ucsd.edu>
2022-04-27 09:57:14 -0700

    py get echo

--- cgi-bin/py-get-echo.py
+++ cgi-bin/py-get-echo.py
@@ -5,6 +5,8 @@ print ("Content-type:text/html\r\n\r\n")
 #urlparse('//www.cwi.nl:80/%7Eguido/Python.html')
 #path_info = request.META.get('PATH_INFO')
 for param in os.environ.keys():
-       print ("<b>%20s</b>: %s<\br>" % (param, os.environ[param]))
+       print ('<p>')
+       print ("hi")
+       print ('</p>')
 
 #test

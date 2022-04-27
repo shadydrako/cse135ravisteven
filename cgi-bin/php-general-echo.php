1856845247fe3e38650b9a39626bee7ca7099132
nguyens00047 <53798291+nguyens00047@users.noreply.github.com>
2022-04-27 16:56:06 -0700

    gen echo

--- cgi-bin/php-general-echo.php
+++ cgi-bin/php-general-echo.php
@@ -2,7 +2,7 @@
 echo "<b> Request Method: </b>";
 echo $_SERVER['REQUEST_METHOD'];
 echo "<b> Protocol </b>";
-echo $_SERVER['SERVER_PROTOCOL']; 
+echo $_SERVER['SERVER_PROTOCOL'];  
 echo "<b> Query String: </b>"; 
 echo $_SERVER['QUERY_STRING'];
 ?>

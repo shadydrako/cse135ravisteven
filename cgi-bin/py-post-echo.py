4c1bec8390e44099b28b9aacae9233de59dfac9f
shadydrako <rtapia@ucsd.edu>
2022-04-27 11:38:37 -0700

    want only head lmfao

--- cgi-bin/py-post-echo.py
+++ cgi-bin/py-post-echo.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+import requests
+
+response = requests.post('https://cse135ravisteven.site/cgi-bin/py-post-echo.py')
 
 print ("Content-type:text/html\r\n\r\n")
 print ('<html>')
 print ('<body>')
 print ('<h1>')
 print ( 'POST ECHO PYTHON')
+print ('<p>')
+print ("response.request.body")
+print ('<p>')
 print ('</h1>')
 
 print ('</body>')

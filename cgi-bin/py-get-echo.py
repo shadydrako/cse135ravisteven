e28e3bc84c4e316491556198c8042a92c20df534
shadydrako <rtapia@ucsd.edu>
2022-04-27 00:55:19 -0700

    test

--- cgi-bin/py-get-echo.py
+++ cgi-bin/py-get-echo.py
@@ -3,9 +3,10 @@ from urllib.parse import urlparse
 print ("Content-type:text/html\r\n\r\n")
 
 urlparse('//www.cwi.nl:80/%7Eguido/Python.html')
-
+print('<p>')
 print(ParseResult(scheme='', netloc='www.cwi.nl:80', path='/%7Eguido/Python.html',
             params='', query='', fragment=''))
+print('</p>')
 
 #urlparse('www.cwi.nl/%7Eguido/Python.html')
 #ParseResult(scheme='', netloc='', path='www.cwi.nl/%7Eguido/Python.html',

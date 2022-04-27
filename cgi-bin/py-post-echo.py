7dc277506145574165200c608c7b38f66e94dbc0
shadydrako <rtapia@ucsd.edu>
2022-04-27 11:30:01 -0700

    test echo py again

--- cgi-bin/py-post-echo.py
+++ cgi-bin/py-post-echo.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3
-import requests
+from datetime import datetime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+
+
+time = datetime.now()
+print ("Content-type:text/html\r\n\r\n")
+print ('<html>')
+print ('<head>')
+print ('<title> Hello World - Python!! </title>')
+print ('</head>')
+print ('<body>')
+print ('<h2>Hello World! This is my first CGI program</h2>')
+print ('<p>')
+print (time.strftime("%H:%M:%S"))
+print ('<p>')
+print ('<p>')
+print ((s.getsockname()[0]))
+print ('<p>')
+print ('</body>')
+print ('</html>')

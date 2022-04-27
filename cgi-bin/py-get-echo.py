c02377ec75ff8652c11cfaea478d66399515aeb2
shadydrako <rtapia@ucsd.edu>
2022-04-27 00:44:00 -0700

    testing get echo

--- cgi-bin/py-get-echo.py
+++ cgi-bin/py-get-echo.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+from urllib.parse import urlparse
+
+URL = request.META.get('HTTP_REFERER')
+
+parsedURL = urlparse(URL)
+print ("Content-type:text/html\r\n\r\n")
+print (<p>)
+print( parsedURL)
+print (</p>)

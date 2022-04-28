62405947078768d0fd63204d85aff97403999a32
shadydrako <rtapia@ucsd.edu>
2022-04-27 18:10:33 -0700

    delete comments

--- cgi-bin/py-get-echo.py
+++ cgi-bin/py-get-echo.py
@@ -8,8 +8,7 @@ import os
 
 print ("Content-type:text/html\r\n\r\n")
 
-#urlparse('//www.cwi.nl:80/%7Eguido/Python.html')
-#path_info = request.META.get('PATH_INFO')
+
 URLQUERY = os.environ['QUERY_STRING']
 URL = 'https://cse135ravisteven.site/'+ os.environ['REQUEST_URI']
 URLagain = urlparse(URL)

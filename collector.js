0c890c73da7da2239fb0141ded19170b06781669
shadydrako <rtapia@ucsd.edu>
2022-05-15 15:14:52 -0700

    getting user language

--- collector.js
+++ collector.js
@@ -1,17 +1,33 @@
+/*
+Static (collected after the page has loaded)
+user agent string (DONE)
+the user's language
+if the user accepts cookies
+if the user allows JavaScript (you will have to manually figure this one out)
+if the user allows images  (you will have to manually figure this one out)
+if the user allows CSS (you will have to manually figure this one out)
+User's screen dimensions
+User's window dimensions
+User's network connection type
+*/
+
 //w3schools
 let userString = navigator.userAgent;
 
 localStorage.setItem('user_agent_string',userString);
 
+let userLanguage = navigator.userLanguage;
+
+localStorage.setItem('user_language',userString);
+
+
 // sending the user String a json data
 //the user's agent
-let agent = navigator.userAgent;
+/*
 //the user's language
-<script> 
         var userLanguage = navigator.userLanguage || navigator.language;
         document.write(
             "The User's preferred language is: "+ userlanguage);
-    </script> 
 //if the user accepts cookies
 if(navigator.cookieEnabled) {
     document.write("Cookies are enabled")
@@ -33,3 +49,4 @@ var height = window.innerHeight||  document.body.clientHeight|| document.documen
 
 
 //User's network connection type
+*/

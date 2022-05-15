02e61e59d655f29a1681aa1c7a2155ca210d7dc3
shadydrako <rtapia@ucsd.edu>
2022-05-15 15:32:22 -0700

    hi

--- collector2.js
+++ collector2.js
@@ -1,3 +1,18 @@
+/*
+Static (collected after the page has loaded)
+user agent string (DONE)
+the user's language (DONE)
+if the user accepts cookies
+if the user allows JavaScript (you will have to manually figure this one out)
+if the user allows images  (you will have to manually figure this one out)
+if the user allows CSS (you will have to manually figure this one out)
+User's screen dimensions
+User's window dimensions
+User's network connection type
+
+
+*/
+
 //w3schools
 let userString = navigator.userAgent;
 
@@ -9,3 +24,8 @@ localStorage.setItem('user_agent_string',userString);
 let userLanguage = navigator.userLanguage || navigator.language;;
 
 localStorage.setItem('user_language', userLanguage);
+
+
+
+let userCookieEnabled = navigator.cookieEnabled;
+localStorage.setItem('user_cookie_enabled',userCookieEnabled );

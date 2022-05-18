4033f5e30458210291abc084f97463a29f2e3c5d
nguyens00047 <53798291+nguyens00047@users.noreply.github.com>
2022-05-18 07:27:43 -0700

    timing

--- collector2.js
+++ collector2.js
@@ -64,14 +64,14 @@ setTimeout(function(){
     let pageLoad = timing[0].loadEventEnd - timing[0].responseEnd;
     localStorage.setItem('timing_page_load', pageLoad);
     //Specifically when the page started loading
-let pageStart = timing[0].domContentLoadedEventStart;
-localStorage.setItem('page_start_load_time', pageStart);
-//Specifically when the page ended loading
-let pageEnd = timing[0].domContentLoaded;
-localStorage.setItem('page_end_time', pageEnd);
-//The total load time (manually calculated - in milliseconds)
-let totalLoad = timing[0].duration; //returns timestamp in milliseconds
-localStorage.setItem('total_load_time',totalLoad); 
+    let pageStart = timing[0].domContentLoadedEventStart;
+    localStorage.setItem('page_start_load_time', pageStart);
+    //Specifically when the page ended loading
+    let pageEnd = timing[0].loadEventEnd;
+    localStorage.setItem('page_end_time', pageEnd);
+    //The total load time (manually calculated - in milliseconds)
+    let totalLoad = timing[0].duration; //returns timestamp in milliseconds
+    localStorage.setItem('total_load_time',totalLoad); 
 }, 5000);
 //let pageLoad = timing[0].loadEventEnd - timing[0].responseEnd;
 //localStorage.setItem('timing_page_load', pageLoad);

98ae21ecec9904a9bab8f1db0f97e9ae721be63c
shadydrako <rtapia@ucsd.edu>
2022-05-18 02:54:34 -0700

    fdd

--- index.js
+++ index.js
@@ -3,19 +3,33 @@
 //how to define a route, node js search up 
 
 //
-var http = require('http');
-var https = require('https');
-const express = require('express');
-const app = express();
+//var http = require('http');
+//var https = require('https');/
+//const express = require('express');
+//const app = express();
 
-const router = express.Router()
+//const router = express.Router()
+
+//app.get('/', (req, res) => {
+  //  res.send('hello world')
+  //});
+
+//app.use(router);
+
+
+  //http.createServer(app).listen(3001);
+  //https.createServer(app).listen(3000);
+
+const express = require('express')
+const app = express()
+const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('hello world')
-  });
+  res.send('Hello World!')
+})
 
-app.use(router);
-
-
-  http.createServer(app).listen(3001);
-  https.createServer(app).listen(3000);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+  
+  

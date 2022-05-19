70ab9db375f4491e2f3206cb5800006e614779e2
shadydrako <rtapia@ucsd.edu>
2022-05-18 18:53:51 -0700

    idle

--- collector2.js
+++ collector2.js
@@ -277,7 +277,7 @@ function clearTimer(){
     clearTimeout(idleTime);
 }
 };
-// setInterval(timeInactive, 10000);
+
 
 
 // Any idle time where no activity happened for a period of 2 or more seconds:

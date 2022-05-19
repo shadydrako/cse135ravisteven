82b57a04b4ea1cd541386efbb6b2f2e249720614
shadydrako <rtapia@ucsd.edu>
2022-05-18 21:39:55 -0700

    fsd

--- collector2.js
+++ collector2.js
@@ -361,4 +361,12 @@ fetch("https://cse135ravisteven.site/json/static", requestOptions)
     .then(response => response.text())
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
+    fetch("https://cse135ravisteven.site/json/activity", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+    fetch("https://cse135ravisteven.site/json/performance", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
 }

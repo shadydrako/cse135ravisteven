21713c4b9ff9f0c64dac1be747ffb9bc9f988402
shadydrako <rtapia@ucsd.edu>
2022-05-18 19:34:42 -0700

    f

--- collector2.js
+++ collector2.js
@@ -76,10 +76,9 @@ var requestOptions = {
   method: 'POST',
   headers: myHeaders,
   body: urlencoded,
-  redirect: 'follow'
 };
 
-fetch("http://cse135ravisteven.site/json/static", requestOptions)
+fetch("https://cse135ravisteven.site/json/static", requestOptions)
   .then(response => response.text())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));

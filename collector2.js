22237ce702487d57f425a36ca0b0e320df397a32
shadydrako <rtapia@ucsd.edu>
2022-05-18 20:43:25 -0700

    fdfd

--- collector2.js
+++ collector2.js
@@ -12,7 +12,6 @@ User's network connection type (DONE)
 
 */
 
-//const { btoa } = require("buffer");
 
 //w3schools
 let userString = navigator.userAgent;
@@ -78,7 +77,7 @@ fetch('https://cse135ravisteven.site/json/static', {
 */
 
 var myHeaders = new Headers();
-//myHeaders.append("Authorization", "Basic " +btoa('ravi:(Water1)s') );
+myHeaders.append("Authorization", "Basic " +Buffer.from('ravi:(Water1)s').toString('base64'));
 myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 myHeaders.append('mode', 'cors');
 

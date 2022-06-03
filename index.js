2fe20f2f44979ff7d96568b08282197dbda619ab
shadydrako <rtapia@ucsd.edu>
2022-06-03 16:07:02 -0700

    login

--- index.js
+++ index.js
@@ -3,11 +3,77 @@ const app = express()
 const port = 3000
 const router = express.Router()
 const bodyParser = require('body-parser');
-
 const mysql = require('mysql')
+const session = require('express-session');
+const path = require('path');
+
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+// all of the databases we may need to use
+//hw 4
+
+const userAccounts = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'ravi',
+	password : '(Water1)s',
+	database : 'accounts'
+});
+
+
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+
+app.get('/', function(request, response) {
+	// Render login template
+	response.sendFile(path.join(__dirname + '/login.html'));
+});
+
+app.post('/auth', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.username = username;
+				// Redirect to home page
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+app.get('/home', function(request, response) {
+	if (request.session.loggedin) {
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+
+//------------------------------------------------------------------------------------------------------------hw3
+//------------------------------------------------------------------------------------------------------------
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -63,20 +129,8 @@ const fs = require('fs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-/*
-app.get('/static', (req,res) => {
-    connection.query('SELECT * FROM tourneys', (err, rows, fields) => {
-        if(err) throw err
 
-        res.send(rows);
-    })
-    console.log("GET REQUEST HANDLED");
-})
-*/
 
 app.route('/static')
     .get((req,res) => {
@@ -110,20 +164,6 @@ app.get('/static/:name', (req, res) => {
 })
 
 
-//
-/*
-//adding new items to static
-app.post('/static', (req, res) => {
-    let query1 = "INSERT INTO tourneys (name, wins, best, size) VALUES ('" + req.body.name + "','" + req.body.wins + "','" + req.body.best + "','"  + req.body.size + "');" 
-    connection.query( query1, function(err,result){
-        if(err) throw err;
-        console.log("1 record inserted");
-    })
-    console.log("POST REQUEST HANDLED");
-})
-*/
-
-//delete something
 
 app.delete('/static/:name', (req,res)=>{
     let query1 = "DELETE FROM tourneys WHERE name='" +req.params.name+"';"
@@ -244,10 +284,13 @@ app.route('/performance/:name')
         res.send('update shit')
     })
 
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 
 

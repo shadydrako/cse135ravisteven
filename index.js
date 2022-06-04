80b92bc0378e7d87e98c2349796b88f2e9592407
shadydrako <rtapia@ucsd.edu>
2022-06-03 17:03:39 -0700

    fwfwfwf

--- index.js
+++ index.js
@@ -35,33 +35,35 @@ app.get('/', function(request, response) {
 	response.sendFile(path.join(__dirname + '/login.html'));
 });
 
-app.post('/', function(request, response) {
-	// Capture the input fields
-	let username = request.body.username;
-	let password = request.body.password;
-	// Ensure the input fields exists and are not empty
-	if (username && password) {
-		// Execute SQL query that'll select the account from the database based on the specified username and password
-		userAccounts.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-			// If there is an issue with the query, output the error
-			if (error) throw error;
-			// If the account exists
-			if (results.length > 0) {
-				// Authenticate the user
-				request.session.loggedin = true;
-				request.session.username = username;
-				// Redirect to home page
-                response.redirect('https://cse135ravisteven.site/')
-				response.send("FOund USER")
-			} else {
-				response.send('Incorrect Username and/or Password!');
-			}			
-			response.end();
-		});
-	} else {
-		response.send('Please enter Username and Password!');
-		response.end();
-	}
+app.post('/auth', (request, response)=> {
+
+    response.send("HELP");
+	// // Capture the input fields
+	// let username = request.body.username;
+	// let password = request.body.password;
+	// // Ensure the input fields exists and are not empty
+	// if (username && password) {
+	// 	// Execute SQL query that'll select the account from the database based on the specified username and password
+	// 	userAccounts.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+	// 		// If there is an issue with the query, output the error
+	// 		if (error) throw error;
+	// 		// If the account exists
+	// 		if (results.length > 0) {
+	// 			// Authenticate the user
+	// 			request.session.loggedin = true;
+	// 			request.session.username = username;
+	// 			// Redirect to home page
+    //             response.redirect('https://cse135ravisteven.site/')
+	// 			response.send("FOund USER")
+	// 		} else {
+	// 			response.send('Incorrect Username and/or Password!');
+	// 		}			
+	// 		response.end();
+	// 	});
+	// } else {
+	// 	response.send('Please enter Username and Password!');
+	// 	response.end();
+	// }
 });
 
 app.get('/home', function(request, response) {

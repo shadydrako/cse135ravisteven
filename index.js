406c992364db4a66d4dba1a3cd5296ee1ad4df0f
shadydrako <rtapia@ucsd.edu>
2022-06-03 17:29:17 -0700

    revert

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const router = express.Router()
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 const mysql = require('mysql')
 

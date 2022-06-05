98e0c5a675cf6a0242836abadcc5bad7877afeb5
shadydrako <rtapia@ucsd.edu>
2022-06-05 02:57:24 -0700

    scratch

--- login.js
+++ login.js
@@ -1,74 +1,87 @@
-if( process.env.NODE_ENV !== 'production'){
-	require('dotenv').config();
-}
-
-const express = require('express')
-const app = express()
-const bcrypt = require('bcrypt')
-const passport = require('passport')
-const flash = require('express-flash')
-const session = require('express-session')
-const methodOverride = require('method-override')
-
-const initializePassport = require('./passport-config')
-initializePassport(
-	passport, 
-	email => users.find( user = user.email === email))
-
-
-app.set('view-engine', 'ejs')
-app.use(express.urlencoded({extended: false }))
-app.use( flash())
-
-app.use(session({
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config()
+  }
+  
+  const express = require('express')
+  const app = express()
+  const bcrypt = require('bcrypt')
+  const passport = require('passport')
+  const flash = require('express-flash')
+  const session = require('express-session')
+  const methodOverride = require('method-override')
+  
+  const initializePassport = require('./passport-config')
+  initializePassport(
+	passport,
+	email => users.find(user => user.email === email),
+	id => users.find(user => user.id === id)
+  )
+  
+  const users = []
+  
+  app.set('view-engine', 'ejs')
+  app.use(express.urlencoded({ extended: false }))
+  app.use(flash())
+  app.use(session({
 	secret: process.env.SESSION_SECRET,
 	resave: false,
-	saveUnitialized: false
-}))
-
-app.use(passport.initialize())
-app.use(passport.session())
-
-const users = []
-
-/*routes*/
-
-//this shall load the login page
-app.get('/login', (req, res, next) => {
-        res.render('login.ejs')
-});
-
-app.post('/login', passport.authenticate('local', {
-	//where to go if success
-	successRedirect: '/api/dashboard',
-	failureRedirect: '/api/login',
+	saveUninitialized: false
+  }))
+  app.use(passport.initialize())
+  app.use(passport.session())
+  app.use(methodOverride('_method'))
+  
+  app.get('/', checkAuthenticated, (req, res) => {
+	res.render('index.ejs', { name: req.user.name })
+  })
+  
+  app.get('/login', checkNotAuthenticated, (req, res) => {
+	res.render('login.ejs')
+  })
+  
+  app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+	successRedirect: '/',
+	failureRedirect: '/login',
 	failureFlash: true
-}))
-
-app.get('/register', (req, res) => {
-	res.render('register.ejs');
-})
-
-app.post('/register', async (req, res)=>{
+  }))
+  
+  app.get('/register', checkNotAuthenticated, (req, res) => {
+	res.render('register.ejs')
+  })
+  
+  app.post('/register', checkNotAuthenticated, async (req, res) => {
 	try {
-		//hashes password
-		const hashedPassword = await bcrypt.hash(req.body.password, 10);
-		users.push({
-			id: Date.now().toString(),
-			name: req.body.email,
-			password: hashedPassword
-		})
-		res.redirect('/api/login')
+	  const hashedPassword = await bcrypt.hash(req.body.password, 10)
+	  users.push({
+		id: Date.now().toString(),
+		name: req.body.name,
+		email: req.body.email,
+		password: hashedPassword
+	  })
+	  res.redirect('/login')
 	} catch {
-		res.redirect('/api/register')
+	  res.redirect('/register')
 	}
-	console.log(users);
-})
-// this fucking works lets go
-app.get('/dashboard', (req, res, next) => {
-	res.render('dashboarduser.ejs');
-});
-
-app.listen(3000, function() {
-    console.log('App listening on port 3000!')
-});
+  })
+  
+  app.delete('/logout', (req, res) => {
+	req.logOut()
+	res.redirect('/login')
+  })
+  
+  function checkAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) {
+	  return next()
+	}
+  
+	res.redirect('/login')
+  }
+  
+  function checkNotAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) {
+	  return res.redirect('/')
+	}
+	next()
+  }
+  
+  app.listen(3000)

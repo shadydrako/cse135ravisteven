666fdfde63a75f8fa31fed0c5e932da5e7901686
shadydrako <rtapia@ucsd.edu>
2022-06-05 02:13:32 -0700

    g4rgergerg

--- login.js
+++ login.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
 const app = express()
 
 
 app.set('view-engine', 'ejs')
+app.use(express.urlencoded({extended: false }))
+
+const users = []
 
 /*routes*/
 
@@ -10,6 +14,26 @@ app.set('view-engine', 'ejs')
 app.get('/login', (req, res, next) => {
         res.render('login.ejs')
 });
+
+app.get('/register', (req, res) => {
+	res.render('register.ejs');
+})
+
+app.post('register', (req, res)=>{
+	try {
+		//hashes password
+		const hashedPassword = await bcrypt.hash(req.body.password, 10);
+		users.push({
+			id: Date.now().toString(),
+			name: req.body.name,
+			password: hashedPassword
+		})
+		res.redirect('/api/login')
+	} catch {
+		res.redirect('/api/register')
+	}
+	console.log(users);
+})
 // this fucking works lets go
 app.get('/dashboard', (req, res, next) => {
 	res.render('dashboarduser.ejs');

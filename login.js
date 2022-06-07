135b4c466c4c5acae1e74d4e40985aecd5bcd14c
shadydrako <rtapia@ucsd.edu>
2022-06-06 23:46:01 -0700

    test register login.js

--- login.js
+++ login.js
@@ -2,9 +2,26 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '(Water1)s',
+  database: 'login'
+})
+
+db.connect( (error) => {
+  if(error){
+    console.log("Error connecting")
+  }else{
+    console.log("Database connection successful");
+  }
+})
+
 //using views lfg
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/', express.static(path.join(__dirname, 'static')))
 
@@ -14,6 +31,11 @@ app.get('/login', (req, res) => {
 
 app.get('/register', (req, res) => {
   res.render('register');
+})
+
+app.post('/register', async (req, res) => {
+  console.log(req.body);
+  res.json({status: 'ok'})
 })
 
 app.listen(3000, () => {

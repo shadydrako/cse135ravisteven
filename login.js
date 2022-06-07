b4958cbccd83f3f97886a7bc297574cba6b459c5
shadydrako <rtapia@ucsd.edu>
2022-06-06 23:05:49 -0700

    login.js

--- login.js
+++ login.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const path = require('path');
 
+//using views lfg
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 const app = express();
 
 app.use('/', express.static(path.join(__dirname, 'static')))
+
+app.get('/login', (req, res) => {
+  res.render('login');
+})
 
 app.listen(3000, () => {
   console.log("We are listening");

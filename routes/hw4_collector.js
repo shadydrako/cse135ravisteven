407700566a0ff69761133ac5f2b8dc44abacf857
shadydrako <rtapia@ucsd.edu>
2022-06-08 14:12:51 -0700

    collector data

--- routes/hw4_collector.js
+++ routes/hw4_collector.js
@@ -13,19 +13,19 @@ function ready(startTime){
     let userString = navigator.userAgent;
     localStorage.setItem('user_agent_string',userString);
 
-    let userLanguage = navigator.userLanguage;
+    let userLanguage = navigator.userLanguage || navigator.language;
     localStorage.setItem('user_language', userLanguage);
     
     let userCookieEnabled = navigator.cookieEnabled;
     localStorage.setItem('user_cookie_enabled',userCookieEnabled );
 
-    let screenDimensionWidth = "Screen Width: " + window.screen.width;
-    let screenDimensionHeight = "Screen Height: " + window.screen.height;
+    let screenDimensionWidth = window.screen.width;
+    let screenDimensionHeight = window.screen.height;
     localStorage.setItem('user_screen_dimension_width', screenDimensionWidth);
     localStorage.setItem('user_screen_dimension_height', screenDimensionHeight);
 
-    let windowDimensionWidth = "User WIndow Width: " + window.innerWidth 
-    let windowDimensionHeight = "User WIndow Height: " + window.innerHeight
+    let windowDimensionWidth = window.innerWidth 
+    let windowDimensionHeight = window.innerHeight
     localStorage.setItem('window_dimension_width', windowDimensionWidth);
     localStorage.setItem('window_dimension_height', windowDimensionHeight);
 

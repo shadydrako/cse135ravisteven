66b12b652393f5b6d0922681d7f6ab6081266d3d
shadydrako <rtapia@ucsd.edu>
2022-06-08 16:52:37 -0700

    test for steven

--- routes/data.js
+++ routes/data.js
@@ -110,35 +110,29 @@ router.post('/users', async (req, res ) =>  {
 
 router.post('/static', (req,res )=>{
 
-    // const data = {
-    //     user_string: req.body.userString,
-    //     user_lang: req.body.userLanguage,
-    //     cookie_en:  req.body.userCookieEnabled, 
-    //     user_sc_width: req.body.screenDimensionWidth, 
-    //     use_sc_height: req.body.screenDimensionHeight,
-    //     window_width: req.body.windowDimensionWidth,
-    //     window_height: req.body.windowDimensionHeight, 
-    //     JS_en: req.body.js_en,
-    //     network_connection: req.body.networkConnection
-    // }
-    var items;
-
-    for (const property in req.body) {
-
-        console.log(`${property}: ${req.body[property]}`);
-        console.log('potato');
+    const data = {
+        user_string: req.body.userString,
+        user_lang: req.body.userLanguage,
+        cookie_en:  req.body.userCookieEnabled, 
+        user_sc_width: req.body.screenDimensionWidth, 
+        use_sc_height: req.body.screenDimensionHeight,
+        window_width: req.body.windowDimensionWidth,
+        window_height: req.body.windowDimensionHeight, 
+        JS_en: req.body.js_en,
+        network_connection: req.body.networkConnection
     }
 
-    console.log(items);
 
-    // (user_string,user_lang, cookie_en, user_sc_width, use_sc_height, window_width, window_height, JS_en, network_connection) VALUES ?
-    // db.query('INSERT INTO static SET ? ', {user_string: }, (error, result)=>{
-    //     if(error){
-    //         console.log(error)
-    //     }else{
-    //         console.log("Completed Insertion!");
-    //     }
-    // })
+    console.log(data);
+
+    (user_string,user_lang, cookie_en, user_sc_width, use_sc_height, window_width, window_height, JS_en, network_connection) VALUES ?
+    db.query('INSERT INTO static SET ? ', {user_string: }, (error, result)=>{
+        if(error){
+            console.log(error)
+        }else{
+            console.log("Completed Insertion!");
+        }
+    })
 
 })
 
